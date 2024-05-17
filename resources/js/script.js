db09a7ddeab0d4fdd60a44b8765691c8d03f3e52
Liam Ha <haduylinh2005.work@gmail.com>
2024-05-17 10:41:25 +0700

    fix navbar + add hover, fix overflow-x footer

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const navbarOptions = document.querySelector('.navbar--options');
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    const mainContent = document.querySelector('main');
+
 
     hamburger.addEventListener('click', function() {
         navbarOptions.classList.toggle('active');
+        hamburger.classList.toggle('active');
+        mainContent.classList.toggle('blurred'); // Add this line
     });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
     window.addEventListener("scroll", function() {
         if (window.scrollY > 1000) {
